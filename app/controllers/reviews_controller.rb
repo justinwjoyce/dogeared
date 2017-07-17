0e7a64fca401638b12e0fbfd0749bf4eb32244b5
Justin Joyce <justinwjoyce@gmail.com>
2017-07-17 14:58:48 -0500

    added review methods and raty assets

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,7 @@
 class ReviewsController < ApplicationController
 	before_action :find_book
+	before_action :find_review, only: [:edit, :update, :destroy]
+	before_action :authenticate_user!, only: [:new, :edit]
 
 	def new
 		@review = Review.new
@@ -17,13 +19,34 @@ class ReviewsController < ApplicationController
 		end
 	end
 
+	def edit
+	end
+
+	def update
+		if @review.update(review_params)
+			redirect_to book_path(@book)
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@review.destroy
+		redirect_to book_path(@book)
+	end
+
 	private
 
-	def review_params
-		params.require(:review).permit(:rating, :comment)
-	end
+		def review_params
+			params.require(:review).permit(:rating, :comment)
+		end
 
-	def find_book
-		@book = Book.find(params[:book_id])
-	end
+		def find_book
+			@book = Book.find(params[:book_id])
+		end
+
+		def find_review
+			@review = Review.find(params[:id])
+		end
+
 end
